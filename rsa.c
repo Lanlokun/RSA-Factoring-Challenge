699ed71035a2a862795b5d650df427e829ef1eea
lanlokun <mlanlokun@gmail.com>
2022-12-25 00:39:49 +0000

    RSA Factoring Challenge

--- rsa.c
+++ rsa.c
@@ -1,4 +1,5 @@
 #include "factor.h"
+#include "factorize.c"
 
 
 /**
@@ -8,26 +9,6 @@
  * Return: void
  */
 
- int factorize(char *buffer)
-{
-
-	u_int32_t num;
-	u_int32_t i;
-
-	num = atoi(buffer);
-
-
-	for (i = 2; i < num; i++)
-	{
-		if (num % i == 0)
-		{
-			printf("%d=%d*%d\n",num,num/i,i);
-			break;
-		}
-	}
-
-return (0);
-}
 int main(int argc, char *argv[])
 {
 	FILE *fptr;
